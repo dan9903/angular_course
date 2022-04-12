5140d6a167d3625cccdbb469eabfddd702bd07d5
dan9903 <mnssd2@gmail.com>
2022-04-12 13:45:17 -0300

    end of session

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,43 @@
+import { EventEmitter, Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipeService {
+  recipeSelectedEvent = new EventEmitter<Recipe>();
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Tasty Schnitzel',
+      'This is a simply test',
+      'https://c.stocksy.com/a/xym500/z9/1379931.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Recipe(
+      'Big Fat Burger',
+      'What else you need to say?',
+      'https://media-cdn.tripadvisor.com/media/photo-p/0c/9f/b4/11/kimchi-burger-with-bacon.jpg',
+      [
+        new Ingredient('Bread', 1),
+        new Ingredient('Bacon', 1),
+        new Ingredient('Egg', 1),
+        new Ingredient('Pork', 0.5),
+        new Ingredient('Burger Meat',1),
+        new Ingredient('Cheese', 1)
+      ])
+  ];
+
+  constructor(private shoppingListService: ShoppingListService) {}
+
+  getRecipes(): Recipe[] {
+    return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
+  }
+}
---
+++ src/app/shopping-list/shopping-list.service.ts
@@ -0,0 +1,27 @@
+import { Injectable, EventEmitter } from '@angular/core';
+import { Ingredient } from "../shared/ingredient.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShoppingListService {
+  ingredientChangeEvent = new EventEmitter<Ingredient[]>();
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10)
+  ];
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientChangeEvent.emit(this.ingredients.slice());
+  }
+
+  addIngredients(ingredients: Ingredient[]) {
+    this.ingredients.push(...ingredients);
+    this.ingredientChangeEvent.emit(this.ingredients.slice());
+  }
+
+  getIngredients(){
+    return this.ingredients.slice();
+  }
+}
